9bee950b09d8b035c0b1e4056890dd2c8a0b3a18
Dicarbene <1041641731@qq.com>
2022-07-25 21:56:29 +0800

    Update useUserStore.ts

--- src/stores/useUserStore.ts
+++ src/stores/useUserStore.ts
@@ -6,7 +6,7 @@ interface UserData {
   company: string | null;
   dept: string | null;
   DESCRIPTION: string | null;
-  token: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VybmFtZSI6IjEzIiwicHciOiJDYXJib24ifQ.C5s4GCBwtGxV9ZZ7ivZaLbM5gYEfkD-qX3nm01TTGn8";
+  token: string;
 }
 
 export const useUserStore = defineStore({
